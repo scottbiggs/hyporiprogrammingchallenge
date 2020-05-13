5f6efc94761919d95dfdc5c4730ca1ed4982321c
sbiggs <scott.biggs@consensus-builder.com>
2020-05-13 07:43:09 -0500

    cleaning up a bit

--- app/src/main/java/com/sleepfuriously/hyporiprogrammingchallenge/model/MovieRequester.kt
+++ app/src/main/java/com/sleepfuriously/hyporiprogrammingchallenge/model/MovieRequester.kt
@@ -34,6 +34,16 @@ object MovieRequester {
     //  functions
     //---------------------------------
 
+    /**
+     * Use this to get a list of movies.
+     *
+     * @param   ctx     Can't do anything without this!
+     *
+     * @param   movieCallback   The function to call once the list is ready.
+     *                          It will have a parameter movieList that
+     *                          holds a list [Movie]s.  If there is an error,
+     *                          movieList will be null.
+     */
     fun getMovies(ctx: Context, movieCallback: (movieList: List<Movie>?) -> Unit) {
 
         // setup a volley request
@@ -43,20 +53,22 @@ object MovieRequester {
 
             // successful response
             Response.Listener { response ->
-                Log.d(TAG, "success!")
                 val movieList = mutableListOf<Movie>()
 
+                // parse the json until we get to the movies (have to dig 1 level).
                 val jsonMovieList = response.getJSONArray("results")
+
                 val length = response.getInt("count")
                 if (length != jsonMovieList.length()) {
                     Log.e(TAG, "incompatible lengths in jsonMovieList. Expected $length, but found ${jsonMovieList.length()}")
                     movieCallback.invoke(null)
                 }
 
+                // add each movie to return value
                 for (i in 0 until length) {
                     try {
                         val jsonObject = jsonMovieList.getJSONObject(i)
-                        movieList.add(Movie(jsonObject))
+                        movieList.add(Movie(jsonObject))    // return error condition
                     }
                     catch (e: JSONException) {
                         Log.e(TAG, "Unable to parse json array in getMovies()")
